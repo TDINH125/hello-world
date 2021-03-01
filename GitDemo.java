5109fdf4fd86e6473f5c34387eb639e08040ecbb
TDINH125 <79823303+TDINH125@users.noreply.github.com>
2021-02-28 22:45:25 -0700

    Update GitDemo.java
    added name in line 2
    modified addnumber() to add num to previousNumber and return sum
    modified reverseNumber(int num) adding variable reverseNum and having reverseNum store newNumber

--- GitDemo.java
+++ GitDemo.java
@@ -1,5 +1,5 @@
 /*****
- * Edit this line to have your name.
+ * Tam Dinh
  ******/
 public class GitDemo {
     
@@ -13,8 +13,10 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
+        sum = 0;
+        sum = previousNumber + num;
         System.out.print("Add num to previousNumber");
-        return 0;
+        return sum;
     }
     
     /*****
@@ -41,10 +43,13 @@ public class GitDemo {
      *****/
     public double reverseNumber(int num){
         double newNumber = 1;
+        private double reverseNum = 0;
         
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
+            reverseNum = newNumber;
         }
+        
         
         return newNumber;
     }
